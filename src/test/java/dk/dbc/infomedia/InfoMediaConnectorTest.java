8c84d2495faf1774b98ac044da8b9990f7420be1
Anders Thrane Michelsen <atm@dbc.dk>
2019-01-10 12:29:55 +0100

    Add initial test class

---
+++ src/test/java/dk/dbc/infomedia/InfoMediaConnectorTest.java
@@ -0,0 +1,50 @@
+package dk.dbc.infomedia;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import dk.dbc.httpclient.HttpClient;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.client.Client;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class InfoMediaConnectorTest {
+
+    private static WireMockServer wireMockServer;
+    private static String wireMockHost;
+
+    final static Client CLIENT = HttpClient.newClient(new ClientConfig()
+            .register(new JacksonFeature()));
+    static InfoMediaConnector connector;
+
+    @BeforeAll
+    static void startWireMockServer() {
+        wireMockServer = new WireMockServer(options().dynamicPort()
+                .dynamicHttpsPort());
+        wireMockServer.start();
+        wireMockHost = "http://localhost:" + wireMockServer.port();
+        configureFor("localhost", wireMockServer.port());
+    }
+
+    @BeforeAll
+    static void setConnector() {
+        connector = new InfoMediaConnector(CLIENT, wireMockHost, InfoMediaConnector.TimingLogLevel.INFO, "username", "password");
+    }
+
+    @AfterAll
+    static void stopWireMockServer() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void dummy() throws InfoMediaConnectorException {
+        assertThat(true, is(true));
+    }
+}
